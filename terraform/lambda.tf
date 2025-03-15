576539672e9cb7d39539cac8df1fbe616efa64cf
Aradjin <bertal201199@gmail.com>
2025-03-15 15:24:35 +0100

    initial dev lambda commit

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,18 +1,14 @@
-# resource "aws_lambda_function" "test_lambda" {
-#   # If the file is not in the current working directory you will need to include a
-#   # path.module in the filename.
-#   filename      = "lambda_function_payload.zip"
-#   function_name = "lambda_function_name"
-#   role          = aws_iam_role.iam_for_lambda.arn
-#   handler       = "index.test"
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "getDataFromApi.zip"
+  function_name = "getDataFromApi"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
 
-#   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-#   runtime = "nodejs18.x"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
-#   environment {
-#     variables = {
-#       foo = "bar"
-#     }
-#   }
-# }
+  runtime = "python3.8"
+
+}
---
+++ terraform/src/lambda_function.py
@@ -0,0 +1,20 @@
+import urllib3
+import json
+
+def lambda_handler(event, context):
+    http = urllib3.PoolManager()
+    response = http.request("GET", "https://api.coinlore.net/api/tickers/")
+
+    if response.status == 200:
+        data = json.loads(response.data.decode("utf-8"))  # Convertir en JSON
+
+        # Retourner le JSON sous forme de chaîne dans la clé "body"
+        return {
+            "statusCode": 200,
+            "body": json.dumps(data)  # Convertir l'objet Python en JSON pour la réponse
+        }
+
+    return {
+        "statusCode": response.status,
+        "body": json.dumps({"error": "Erreur lors de la récupération des données"})
+    }
