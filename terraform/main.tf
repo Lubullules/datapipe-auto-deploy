4877e29f66e341376a2acaa1c6302954356a40e2
NAPondu <nicolas31dupont@gmail.com>
2025-03-17 11:06:43 +0100

    Add AWS Lambda function for S3 data upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,3 +83,26 @@ resource "aws_lambda_function" "lambda_getDataFromApi_resource" {
 
 }
 
+#Resource allocation for AWS Lambda s3DataUpload
+
+data "archive_file" "lambda_s3DataUpload_data" {
+  type        = "zip"
+  source_file = "${path.module}/../aws/s3DataUpload.py" # Assurez-vous que le code source est dans ce dossier
+  output_path = "${path.module}/../aws/s3DataUpload.zip"
+}
+
+resource "aws_lambda_function" "lambda_s3DataUpload_resource" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "${path.module}/../aws/s3DataUpload.zip"
+  function_name = "s3DataUpload"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+
+  source_code_hash = data.archive_file.lambda_s3DataUpload_data.output_base64sha256
+
+  runtime = "python3.13"
+
+}
+
