e4af9cf47c9978a620fef475771831660053b357
Aradjin <bertal201199@gmail.com>
2025-03-17 09:47:27 +0100

    Fix output path casing for Lambda archive file

--- terraform/ressource.tf
+++ terraform/ressource.tf
@@ -1,6 +1,6 @@
 data "archive_file" "lambda" {
   type        = "zip"
   source_file  = "${path.module}/src/lambda_function.py" # Assurez-vous que le code source est dans ce dossier
-  output_path = "${path.module}/getDataFromAPI.zip"
+  output_path = "${path.module}/getDataFromApi.zip"
 }
 
