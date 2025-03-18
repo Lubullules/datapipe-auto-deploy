e6caaaafd34c9185ccee02149dba4fcae8737dc2
NAPondu <nicolas31dupont@gmail.com>
2025-03-18 18:54:32 +0100

    Comment out scheduled triggers in Terraform workflows and CloudWatch event target resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,9 +67,9 @@ resource "aws_cloudwatch_event_rule" "cloudwatch_event_10min" {
 }
 
 #Affectation of the event trigger to the target Step Function
-resource "aws_cloudwatch_event_target" "step_function_target" {
-  rule      = aws_cloudwatch_event_rule.cloudwatch_event_10min.name
-  target_id = "data_injection_workflow_target"
-  arn       = aws_sfn_state_machine.data_injection_workflow.arn
-  role_arn  = aws_iam_role.iam_cloudwatch_events_role.arn
-}
+# resource "aws_cloudwatch_event_target" "step_function_target" {
+#   rule      = aws_cloudwatch_event_rule.cloudwatch_event_10min.name
+#   target_id = "data_injection_workflow_target"
+#   arn       = aws_sfn_state_machine.data_injection_workflow.arn
+#   role_arn  = aws_iam_role.iam_cloudwatch_events_role.arn
+# }
