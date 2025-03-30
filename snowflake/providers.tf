0b273d9dccc0b60537141acd6400b99532bf5a01
Aradjin <bertal201199@gmail.com>
2025-03-30 16:32:29 +0200

    Update .gitignore, modify AWS backend configuration, and refactor Snowflake provider settings

--- snowflake/providers.tf
+++ snowflake/providers.tf
@@ -6,5 +6,10 @@ terraform {
   }
 }
 provider "snowflake" {
-  role = "SYSADMIN"
+  role = "ACCOUNTADMIN"
+  organization_name = "JLWJSHF"
+  account_name = "BJ18758"
+  user = "TF-SNOW"
+  authenticator = "SNOWFLAKE_JWT"
+  private_key = "${file("~/.ssh/snowflake_tf_snow_key.p8")}"
 }
--- snowflake/remote-state.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "aws" {
-  backend = "s3"
-
-  config = {
-    bucket = var.bucket
-    region = var.region
-    key    = var.key
-  }
-}
