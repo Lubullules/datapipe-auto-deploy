0a6d4a0e4bc012b47b55c94a8453448621af2c39
NAPondu <nicolas31dupont@gmail.com>
2025-04-01 11:35:06 +0200

    Add Snowflake database role and grant privileges in Terraform configuration

--- snowflake/privileges.tf
+++ snowflake/privileges.tf
@@ -1,5 +1,15 @@
-resource "snowflake_grant_privileges_to_database_role" "tf-snow-role_grant" {
-  database_role_name = var.snowflake_role
+resource "snowflake_database_role" "tf-snow-db-role" {
+  name     = "${var.snowflake_role}-DB"
+  database = snowflake_database.my_database.name
+}
+
+resource "snowflake_grant_privileges_to_database_role" "tf-snow-db-role_grant" {
+  database_role_name = snowflake_database_role.tf-snow-db-role.name
   on_database        = snowflake_database.my_database.name
   all_privileges     = true
+}
+
+resource "snowflake_grant_database_role" "tf-snow-role_grant" {
+  database_role_name = snowflake_database_role.tf-snow-db-role.name
+  parent_role_name   = var.snowflake_role
 }
