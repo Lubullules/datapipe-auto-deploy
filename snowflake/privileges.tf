9aa6247692265e4a9b1e8b880d8b34150bb8b55c
NAPondu <nicolas31dupont@gmail.com>
2025-04-01 12:00:37 +0200

    Fix database role name reference in Snowflake privileges configuration

--- snowflake/privileges.tf
+++ snowflake/privileges.tf
@@ -4,12 +4,12 @@ resource "snowflake_database_role" "tf-snow-db-role" {
 }
 
 resource "snowflake_grant_privileges_to_database_role" "tf-snow-db-role_grant" {
-  database_role_name = snowflake_database_role.tf-snow-db-role.name
+  database_role_name = "${snowflake_database.my_database.name}.${snowflake_database_role.tf-snow-db-role.name}"
   on_database        = snowflake_database.my_database.name
   all_privileges     = true
 }
 
 resource "snowflake_grant_database_role" "tf-snow-role_grant" {
-  database_role_name = snowflake_database_role.tf-snow-db-role.name
+  database_role_name = "${snowflake_database.my_database.name}.${snowflake_database_role.tf-snow-db-role.name}"
   parent_role_name   = var.snowflake_role
 }
