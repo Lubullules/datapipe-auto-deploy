f75f066a038ff39dc752caf6b788aeaa089b03e9
Aradjin <bertal201199@gmail.com>
2025-04-02 15:12:35 +0200

    Add outputs for S3 bucket name and Snowpipe role ARN in remote state configuration

--- snowflake/remote-state.tf
+++ snowflake/remote-state.tf
@@ -6,4 +6,9 @@ data "terraform_remote_state" "aws" {
     bucket = var.bucket
     key    = var.aws_key
   }
+
+  outputs = {
+    s3_bucket_name    = aws_s3_bucket.bucket.id
+    snowpipe_role_arn = aws_iam_role.iam_snowpipe_role.arn
+  }
 }
