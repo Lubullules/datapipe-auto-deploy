0735d476ac3785e774137a1e8be069af374cc7e5
NAPondu <nicolas31dupont@gmail.com>
2025-04-02 15:23:25 +0200

    Refactor remote state configuration to use outputs from AWS module

--- snowflake/remote-state.tf
+++ snowflake/remote-state.tf
@@ -1,14 +1,12 @@
+module "aws" {
+  source = "../aws"
+}
+
 data "terraform_remote_state" "aws" {
   backend = "s3"
 
   config = {
-    region = var.region
-    bucket = var.bucket
-    key    = var.aws_key
-  }
-
-  outputs = {
-    s3_bucket_name    = aws_s3_bucket.bucket.id
-    snowpipe_role_arn = aws_iam_role.iam_snowpipe_role.arn
+    s3_bucket_name = module.aws.s3_bucket_name
+    snowpipe_role_arn = module.aws.snowpipe_role_arn
   }
 }
