71b61a654e0f33ec33d25ca85779debfa655a444
NAPondu <nicolas31dupont@gmail.com>
2025-04-02 15:24:42 +0200

    Refactor spacing in remote state configuration for consistency

--- snowflake/remote-state.tf
+++ snowflake/remote-state.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "aws" {
   backend = "s3"
 
   config = {
-    s3_bucket_name = module.aws.s3_bucket_name
+    s3_bucket_name    = module.aws.s3_bucket_name
     snowpipe_role_arn = module.aws.snowpipe_role_arn
   }
 }
