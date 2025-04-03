2b827cc158de471658b3d31e835002f11b675034
NAPondu <nicolas31dupont@gmail.com>
2025-04-03 18:24:43 +0200

    Fix IAM policy for S3 by correcting action syntax for GetObjectVersion and GetBucketLocation

--- aws/iam.tf
+++ aws/iam.tf
@@ -171,7 +171,7 @@ resource "aws_iam_policy" "snowpipe_s3_policy" {
         "Effect" : "Allow",
         "Action" : [
           "s3:GetObject",
-          "s3.GetObjectVersion",
+          "s3:GetObjectVersion",
         ],
         "Resource" : [
           "${aws_s3_bucket.bucket.arn}/processed/*",
@@ -181,7 +181,7 @@ resource "aws_iam_policy" "snowpipe_s3_policy" {
         "Effect" : "Allow",
         "Action" : [
           "s3:ListBucket",
-          "s3.GetBucketLocation",
+          "s3:GetBucketLocation",
         ],
         "Resource" : [
           "${aws_s3_bucket.bucket.arn}",
