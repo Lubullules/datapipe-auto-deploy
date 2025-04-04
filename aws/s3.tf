8a23754eb40cb2aa65a2ac73cd6f5c3abcf19e79
NAPondu <nicolas31dupont@gmail.com>
2025-04-04 10:58:52 +0200

    Add logging configuration to S3 bucket resource

--- aws/s3.tf
+++ aws/s3.tf
@@ -2,4 +2,9 @@
 resource "aws_s3_bucket" "bucket" {
   bucket        = "${var.project_name}-${var.env}-${var.region}"
   force_destroy = true
+
+  logging {
+    target_bucket = "aws-s3-base-bucket-project-test"
+    target_prefix = "${var.project_name}-${var.env}-${var.region}-logs/"
+  }
 }
