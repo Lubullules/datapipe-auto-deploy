4598bc526d7e12f8441e1e7ae94416d4c06783c3
NAPondu <nicolas31dupont@gmail.com>
2025-04-04 15:38:31 +0200

    Refactor SNS topic policy and update Snowflake pipe configuration for improved clarity and consistency

--- aws/sns.tf
+++ aws/sns.tf
@@ -1,45 +1,45 @@
 resource "aws_sns_topic" "snowpipe_topic" {
   name = "${var.project_name}-snowpipe-topic"
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Id": "example-ID",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Id" : "example-ID",
+    "Statement" : [
       {
-        "Sid": "Example SNS topic policy",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "s3.amazonaws.com"
+        "Sid" : "Example SNS topic policy",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "s3.amazonaws.com"
         },
-        "Action": "SNS:Publish",
-        "Resource": "arn:aws:sns:${var.region}:${var.account_id}:${var.project_name}-snowpipe-topic",
-        "Condition": {
-          "StringEquals": {
-            "aws:SourceAccount": "${var.account_id}"
+        "Action" : "SNS:Publish",
+        "Resource" : "arn:aws:sns:${var.region}:${var.account_id}:${var.project_name}-snowpipe-topic",
+        "Condition" : {
+          "StringEquals" : {
+            "aws:SourceAccount" : "${var.account_id}"
           },
-          "ArnLike": {
-            "aws:SourceArn": "${aws_s3_bucket.bucket.arn}"
+          "ArnLike" : {
+            "aws:SourceArn" : "${aws_s3_bucket.bucket.arn}"
           }
         }
       },
       {
-        "Sid": "1",
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": "arn:aws:iam::296062579650:user/jqyv0000-s"
+        "Sid" : "1",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::296062579650:user/jqyv0000-s"
         },
-        "Action": "sns:Subscribe",
-        "Resource": "arn:aws:sns:${var.region}:${var.account_id}:${var.project_name}-snowpipe-topic",
+        "Action" : "sns:Subscribe",
+        "Resource" : "arn:aws:sns:${var.region}:${var.account_id}:${var.project_name}-snowpipe-topic",
       }
     ]
-  })  
+  })
 }
 
 resource "aws_s3_bucket_notification" "snowpipe_s3_notification" {
   bucket = aws_s3_bucket.bucket.id
 
   topic {
-    topic_arn = aws_sns_topic.snowpipe_sns_topic.arn
-    events = ["s3:ObjectCreated:*"]
+    topic_arn     = aws_sns_topic.snowpipe_sns_topic.arn
+    events        = ["s3:ObjectCreated:*"]
     filter_prefix = "processed/"
   }
 }
