22b195706206a06017769ac6a4eeb32a7c681320
NAPondu <nicolas31dupont@gmail.com>
2025-04-14 15:19:50 +0200

    Remove unnecessary wildcard from ARN in Step Functions policy for processRedditData Lambda function

--- aws/iam.tf
+++ aws/iam.tf
@@ -64,7 +64,7 @@ resource "aws_iam_policy" "step_functions_policy" {
           "${aws_lambda_function.lambda_getDataFromApi_resource.arn}",
           "${aws_lambda_function.lambda_processData_resource.arn}",
           "${aws_lambda_function.lambda_getDataFromRedditApi_resource.arn}",
-          "${aws_lambda_function.lambda_processRedditData_resource.arn}:*"
+          "${aws_lambda_function.lambda_processRedditData_resource.arn}"
         ]
       }
     ]
