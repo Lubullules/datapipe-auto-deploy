abc375d395f4fb1108d142639a2bcd7a9a8a9b05
Aradjin <bertal201199@gmail.com>
2025-04-14 12:26:47 +0200

    Add data archive configuration for getDataFromRedditApi Lambda function

--- aws/data.tf
+++ aws/data.tf
@@ -10,3 +10,9 @@ data "archive_file" "lambda_processData_data" {
   source_file = "${path.module}/definitions/processData.py" # Assurez-vous que le code source est dans ce dossier
   output_path = "${path.module}/definitions/processData.zip"
 }
+
+data "archive_file" "lambda_getDataFromRedditApi_data" {
+  type        = "zip"
+  source_file = "${path.module}/definitions/getDataFromRedditApi.py" # Assurez-vous que le code source est dans ce dossier
+  output_path = "${path.module}/definitions/getDataFromRedditApi.zip"
+}
