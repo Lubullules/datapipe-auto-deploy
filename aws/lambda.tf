5243203fb7a9f0531cd68e3fad03ef6f76d3cc13
Aradjin <bertal201199@gmail.com>
2025-04-14 11:51:06 +0200

    Add environment variables for Reddit API access in lambda_getDataFromRedditApi_resource

--- aws/lambda.tf
+++ aws/lambda.tf
@@ -44,3 +44,32 @@ resource "aws_lambda_function" "lambda_processData_resource" {
     }
   }
 }
+
+resource "aws_lambda_function" "lambda_getDataFromRedditApi_resource" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "${path.module}/definitions/getDataFromRedditApi.zip"
+  function_name = "${var.project_name}-${var.env}-getDataFromRedditApi"
+  role          = aws_iam_role.iam_lambda_role.arn
+  handler       = "getDataFromRedditApi.lambda_handler"
+  timeout       = 60
+  memory_size   = 512
+
+  layers = ["arn:aws:lambda:eu-west-1:336392948345:layer:AWSSDKPandas-Python313:1"]
+
+  source_code_hash = data.archive_file.lambda_getDataFromRedditApi_data.output_base64sha256
+
+  runtime = "python3.13"
+
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.bucket.bucket
+
+      reddit_username = "BusinessAd4873"
+      reddit_password = "magodassearthur123"
+      user_agent = "CryptoPostsTest (by /u/BusinessAd4873)"
+      client_id = "ECL9oz9sGlFbawRB95ZcrA"
+      client_secret = "DPGGI-B9u-MAbJX_HDG_rtxTfHaLLQ"
+    }
+  }
+}
