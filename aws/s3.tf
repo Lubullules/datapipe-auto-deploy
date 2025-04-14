f15ea7e70c3e52b25523e96a0f4bc6934421cfc3
NAPondu <nicolas31dupont@gmail.com>
2025-04-14 15:55:06 +0200

    Update .gitignore and S3/Step Functions configurations for improved resource management

--- aws/s3.tf
+++ aws/s3.tf
@@ -1,10 +1,15 @@
+#TODO: Add a description of the purpose of this file and the resources
+
 #Resource allocation for AWS S3 bucket
 resource "aws_s3_bucket" "bucket" {
   bucket        = "${var.project_name}-${var.env}-${var.region}"
   force_destroy = true
+}
 
-  logging {
-    target_bucket = "aws-s3-base-bucket-project-test"
-    target_prefix = "${var.project_name}-${var.env}-${var.region}-logs/"
-  }
+resource "aws_s3_bucket_logging" "data_bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  #TODO: var the base bucket name
+  target_bucket = "aws-s3-base-bucket-project-test"
+  target_prefix = "data_logs/"
 }
--- aws/step-functions.tf
+++ aws/step-functions.tf
@@ -2,10 +2,10 @@ resource "aws_sfn_state_machine" "data_injection_workflow" {
   name     = "${var.project_name}-${var.env}-dataInjectionWorkflow"
   role_arn = aws_iam_role.iam_sfn_role.arn
   definition = templatefile("${path.module}/definitions/DataInjectionWorkflow.asl.json", {
-    get_data_from_coinlore_api_arn        = aws_lambda_function.lambda_getDataFromCoinloreApi_resource.arn,
-    process_data_coinlore_arn             = aws_lambda_function.lambda_processCoinloreData_resource.arn,
-    get_data_from_reddit_api_arn = aws_lambda_function.lambda_getDataFromRedditApi_resource.arn,
-    process_reddit_data_arn      = aws_lambda_function.lambda_processRedditData_resource.arn,
+    get_data_from_coinlore_api_arn = aws_lambda_function.lambda_getDataFromCoinloreApi_resource.arn,
+    process_data_coinlore_arn      = aws_lambda_function.lambda_processCoinloreData_resource.arn,
+    get_data_from_reddit_api_arn   = aws_lambda_function.lambda_getDataFromRedditApi_resource.arn,
+    process_reddit_data_arn        = aws_lambda_function.lambda_processRedditData_resource.arn,
   })
 
   type = "STANDARD"
