45a395c4d49272df5eccc72e14bb269dda6352ac
NAPondu <nicolas31dupont@gmail.com>
2025-05-21 14:58:46 +0200

    Add local Terraform deployment and destruction scripts; update .gitignore

---
+++ .local/localterraformdeploy.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# This script deploys the local terraform infrastructure
+# It is assumed that the project structure is as imported
+#------------------------------------------------------------
+echo "This script deploys the local terraform infrastructure \n"
+echo "Loading local environment variables... \n"
+. "./.env.local"
+
+echo "   Environment is $ENV"
+echo "   Project name is $PROJECT_NAME"
+echo "   AWS region is $AWS_REGION"
+
+echo "   AWS bucket for tfstates is $TF_STATES_BASE_BUCKET_NAME"
+echo "   Key for AWS tfstate is $AWS_TF_STATE_KEY"
+echo "   Key for Snowflake tfstate is $SNOWFLAKE_TF_STATE_KEY"
+
+export TF_VAR_env="$ENV"
+export TF_VAR_project_name="$PROJECT_NAME"
+
+export TF_VAR_aws_region="$AWS_REGION"
+export TF_VAR_base_bucket="$TF_STATES_BASE_BUCKET_NAME"
+
+echo "\n\n\n"
+
+#------------------------------------------------------------
+# AWS
+echo "Deploying AWS infrastructure \n"
+cd "./aws"
+
+echo "   AWS account ID is $AWS_ACCOUNT_ID"
+
+echo "   Snowflake AWS user ARN is $SNOWFLAKE_AWS_USER_ARN"
+
+echo "   Reddit username is $REDDIT_USERNAME"
+echo "   Reddit password is $REDDIT_PASSWORD"
+echo "   Reddit client ID is $REDDIT_CLIENT_ID"
+echo "   Reddit client secret is $REDDIT_CLIENT_SECRET \n"
+
+export TF_VAR_aws_account_id="$AWS_ACCOUNT_ID"
+
+export TF_VAR_snowflake_aws_user_arn="$SNOWFLAKE_AWS_USER_ARN"
+
+export TF_VAR_reddit_username="$REDDIT_USERNAME"
+export TF_VAR_reddit_password="$REDDIT_PASSWORD"
+export TF_VAR_reddit_client_id="$REDDIT_CLIENT_ID"
+export TF_VAR_reddit_client_secret="$REDDIT_CLIENT_SECRET"
+
+terraform fmt --recursive
+terraform init  -backend-config="region=$AWS_REGION" -backend-config="bucket=$TF_STATES_BASE_BUCKET_NAME" -backend-config="key=$AWS_TF_STATE_KEY"
+terraform apply
+
+echo "\n\n\n"
+
+#------------------------------------------------------------
+# Snowflake
+
+echo "Deploying Snowflake infrastructure \n"
+cd "../snowflake"
+
+echo "   Snowflake private key has been loaded from $PRIVATE_KEY_PATH"
+echo "   Snowflake organization name is $SNOWFLAKE_ORG_NAME"
+echo "   Snowflake account name is $SNOWFLAKE_ACCOUNT_NAME \n"
+
+export TF_VAR_aws_tf_state_key="$AWS_TF_STATE_KEY"
+
+export TF_VAR_snowflake_private_key="$SNOWFLAKE_PRIVATE_KEY"
+export TF_VAR_snowflake_org_name="$SNOWFLAKE_ORG_NAME"
+export TF_VAR_snowflake_account_name="$SNOWFLAKE_ACCOUNT_NAME"
+
+terraform fmt --recursive
+terraform init -backend-config="region=$AWS_REGION" -backend-config="bucket=$TF_STATES_BASE_BUCKET_NAME" -backend-config="key=$SNOWFLAKE_TF_STATE_KEY"
+terraform apply
